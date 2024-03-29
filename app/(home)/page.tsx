df7c7a465e1d6650436cdf1a6e525879a83fb4d9
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-03-29 15:29:32 +0100

    project setting

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: 'Home',
+}
+
+export default async function HomePage() {
+  
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  );
+}
